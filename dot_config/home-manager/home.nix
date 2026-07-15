1af44697365a04454feca3755e3e5a76a36eee35
Francois Charih <francois@charih.ca>
2026-07-15 06:24:38 -0400

    Modify

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -6,32 +6,52 @@ let
 in
 {
   home.username = "fcharih";
-  home.homeDirectory = "/home/fcharih";
+  home.homeDirectory =
+    if pkgs.stdenv.isDarwin then "/Users/fcharih" else "/home/fcharih";
   home.stateVersion = "24.11";
   home.packages = with pkgs; [
-  ripgrep
-fd
-htop
-jq
-fzf
-neovim
-uv
-zsh
-eza
-azure-cli-with-extensions
-azure-storage-azcopy
-starship
-oh-my-zsh
-_1password-cli
-chezmoi
-gh
-mise
-podman
-podman-compose
-buildah
-skopeo
-claude-code
-signal-cli
+    ripgrep
+    fd
+    htop
+    jq
+    fzf
+    neovim
+    uv
+    zsh
+    eza
+    azure-cli-with-extensions
+    azure-storage-azcopy
+    starship
+    oh-my-zsh
+    _1password-cli
+    chezmoi
+    gh
+    mise
+    podman
+    podman-compose
+    buildah
+    skopeo
+    claude-code
+    signal-cli
+    pkg-config
+    cmake
+    gcc
+    nushell
+    android-tools
+    libsixel
+    android-tools
+    terraform
+    zellij
+    timg
+    emacs30
+    emacsPackages.doom
+    postgresql_18
+    openssl-dev
+    unzip
   ];
   programs.home-manager.enable = true;
+
+  home.sessionVariables = {
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  };
 }
