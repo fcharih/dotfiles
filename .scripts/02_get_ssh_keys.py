07d5814d28c7c6db4bac8155bb1b51d4b2c929db
Francois Charih <francois@charih.ca>
2026-08-06 20:30:03 -0400

    Update ssh key retrieval

--- .scripts/02_get_ssh_keys.py
+++ .scripts/02_get_ssh_keys.py
@@ -5,24 +5,28 @@ import subprocess as sp
 import re
 import json
 
-session_token = sp.run("op signin --raw", shell=True, capture_output=True, text=True).stdout
-result = sp.run(f"op item list --format=json --tags ssh --session {session_token} | op item get - --fields 'private key,public key' --reveal --format=json --session {session_token}", shell=True, text=True, capture_output=True)
+try:
+#session_token = sp.run("op signin --raw", shell=True, capture_output=True, text=True).stdout
+#result = sp.run(f"op item list --format=json --tags ssh --session {session_token} | op item get - --fields 'private key,public key' --reveal --format=json --session {session_token}", shell=True, text=True, capture_output=True)
+    result = sp.run(f"op item list --format=json --tags ssh | op item get - --fields 'private key,public key' --reveal --format=json", shell=True, text=True, capture_output=True)
 
-keysets = re.findall(r"\[([^\]]+)\]", result.stdout)
+    keysets = re.findall(r"\[([^\]]+)\]", result.stdout)
 
-for keyset in keysets:
-    keyset = json.loads(f"[{keyset}]")
+    for keyset in keysets:
+        keyset = json.loads(f"[{keyset}]")
 
-    for key in keyset:
+        for key in keyset:
 
-        key_name = key['reference'].split("/")[-2]
+            key_name = key['reference'].split("/")[-2]
 
-        if 'private' in key['reference']:
-            filename = f"{pathlib.Path.home()}/.ssh/{key_name}"
-            with open(filename, 'w') as private_key_file:
-                private_key_file.write(key['value'])
-            os.chmod(filename, 0o600)
-        elif 'public' in key['reference']:
-            with open(f"{pathlib.Path.home()}/.ssh/{key_name}.pub", 'w') as public_key_file:
-                public_key_file.write(key['value'])
-
+            if 'private' in key['reference']:
+                filename = f"{pathlib.Path.home()}/.ssh/{key_name}"
+                with open(filename, 'w') as private_key_file:
+                    private_key_file.write(key['value'])
+                os.chmod(filename, 0o600)
+            elif 'public' in key['reference']:
+                with open(f"{pathlib.Path.home()}/.ssh/{key_name}.pub", 'w') as public_key_file:
+                    public_key_file.write(key['value'])
+    print("Keys successfully downloaded!")
+except Exception as e:
+    print("Keys could not be downloaded:", str(e))
